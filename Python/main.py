e967b7c42410176cf3e801a4d4d62b133d0486a8
GuillaumeCariou <guillaume.cariou06@gmail.com>
2021-03-01 12:37:14 +0100

    ajout d'un dossier C++

---
+++ Python/main.py
@@ -0,0 +1,16 @@
+import line
+import cv2
+import time
+
+hist = line.Historique(hist_size=10)
+compteur = 0
+ips = 0
+after = time.time() + 1
+
+if __name__ == '__main__':
+    while True:
+        ips, compteur, after = line.caclulate_ips(ips, compteur, after)
+        angle = line.line_detection(hist, ips) # si ips == 0 alors les ips ne sont pas affiché
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
