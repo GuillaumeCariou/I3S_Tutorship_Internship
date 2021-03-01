a66ce6961a9e4e40350953a1bb8536487ddb47a9
GuillaumeCariou <guillaume.cariou06@gmail.com>
2021-03-01 12:20:06 +0100

    Afficher les ips

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ after = time.time() + 1
 if __name__ == '__main__':
     while True:
         ips, compteur, after = line.caclulate_ips(ips, compteur, after)
-        angle = line.line_detection(hist, 0)
+        angle = line.line_detection(hist, ips) # si ips == 0 alors les ips ne sont pas affiché
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
