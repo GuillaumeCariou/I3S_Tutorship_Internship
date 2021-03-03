305a256491d64429bde0919b39be6078ae1f34ec
GuillaumeCariou <guillaume.cariou06@gmail.com>
2021-03-03 13:53:17 +0100

    afficher la video et retirer le try

--- Python/main.py
+++ Python/main.py
@@ -49,24 +49,27 @@ if __name__ == '__main__':
 
 
 def send_command(left, right):
-    try:
-        cmd = str(left) + ',' + str(right) + ','
-        arduino.write(cmd.encode())
-        time.sleep(0.1)  # wait for arduino to answer
-        """
-        while arduino.inWaiting() == 0: pass
-        if arduino.inWaiting() > 0:
-            answer = arduino.readline()
-            print(answer)
-            arduino.flushInput()  # remove data after reading
-        """
-    except KeyboardInterrupt:
-        print("Send Command")
+    cmd = str(left) + ',' + str(right) + ','
+    arduino.write(cmd.encode())
+    time.sleep(0.1)  # wait for arduino to answer
+    """
+    while arduino.inWaiting() == 0: pass
+    if arduino.inWaiting() > 0:
+        answer = arduino.readline()
+        print(answer)
+        arduino.flushInput()  # remove data after reading
+    """
 
 
 if __name__ == '__main__':
     with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
         time.sleep(0.1)  # wait for serial to open
+
+        video = input("Voulez vous la vidéo ? Y or N ")
+        if video == "Y":
+            video = True
+        else:
+            video = False
         if arduino.isOpen():
             print("{} connected!".format(arduino.port))
 
@@ -75,7 +78,7 @@ if __name__ == '__main__':
                 ret, original = vid.read()
                 ips, compteur, after = line.caclulate_ips(ips, compteur, after)
                 angle, size, img_line_plus_mean = line.line_detection(hist=angle_hist, ips=ips, display_image=False,
-                                                                      display_mean=True,
+                                                                      display_mean=video,
                                                                       original_picture=original)  # si ips == 0 alors les ips ne sont pas affiché
 
                 # print image size once
@@ -84,7 +87,7 @@ if __name__ == '__main__':
                     imprimer_taille_image = False
 
                 # stop the program by pressing q
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                if cv2.waitKey(1) & 0xFF == ord('q') & video:
                     break
 
                 # Reaction to angle
