bfad35a34ae1ccf0f237019c618f4ba561736966
GuillaumeCariou <guillaume.cariou06@gmail.com>
2021-03-03 10:32:26 +0100

    try send Two int

--- Python/serial_test.py
+++ Python/serial_test.py
@@ -15,7 +15,9 @@ if __name__ == '__main__':
             try:
                 while True:
                     cmd = input("Enter command : ")
+                    cmd2 = input("Enter command : ")
                     arduino.write(cmd.encode())
+                    arduino.write(cmd2.encode())
                     # time.sleep(0.1) #wait for arduino to answer
                     while arduino.inWaiting() == 0: pass
                     if arduino.inWaiting() > 0:
